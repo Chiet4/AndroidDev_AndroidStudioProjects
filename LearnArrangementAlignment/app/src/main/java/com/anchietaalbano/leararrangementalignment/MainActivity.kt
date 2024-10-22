e26da4dca1c2c8071a8b77e87ce4d815290d0647
Anchieta N. Albano <anchietaalbano@alu.ufc.br>
2024-10-22 09:33:13 -0300

    Create Alignment & Arrangement in Jetpack Compose

---
+++ LearnArrangementAlignment/app/src/main/java/com/anchietaalbano/leararrangementalignment/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.anchietaalbano.leararrangementalignment
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.anchietaalbano.leararrangementalignment.ui.theme.LearArrangementAlignmentTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LearArrangementAlignmentTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    LearnAlignmentArrangement()
+                }
+            }
+        }
+    }
+}
+
+// Alignment = Cross Axis ( Row = Vertical, Column = Horizontal)
+// Arrangement = Main Axis ( Row = Horizontal, Column = Vertical)
+
+@Composable
+fun LearnAlignmentArrangement(){
+
+    //RowAlignment: Top, CenterVertically, Bottom
+    //RowArrangement: Start, Center, End, SpaceBetween, SpaceAround, SpaceEvenly
+                    // Absolute.Left, Absolute.Right, Absolute.Center
+                    // Absolute.SpaceBetween, Absolute.SpaceAround, Absolute.SpaceEvenly
+
+    
+
+
+}
