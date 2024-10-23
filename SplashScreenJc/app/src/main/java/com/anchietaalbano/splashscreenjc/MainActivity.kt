e85f549e0b3179c23976280cd78aeaabbef7dcd2
Anchieta Albano <111232477+Chiet4@users.noreply.github.com>
2024-10-23 10:09:23 -0300

    test

--- SplashScreenJc/app/src/main/java/com/anchietaalbano/splashscreenjc/MainActivity.kt
+++ SplashScreenJc/app/src/main/java/com/anchietaalbano/splashscreenjc/MainActivity.kt
@@ -45,6 +45,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     SplashScreenJcTheme {
-        Greeting("Android")
+        Greeting("Albano")
     }
 }
