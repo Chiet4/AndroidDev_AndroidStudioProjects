a6c0ee62c20b870ecb769a3418ca997df1f996a6
Anchieta Albano <111232477+Chiet4@users.noreply.github.com>
2024-10-25 15:04:16 -0300

    add state mutable

--- LearnState/app/src/main/java/com/example/learnstate/MainActivity.kt
+++ LearnState/app/src/main/java/com/example/learnstate/MainActivity.kt
@@ -1,16 +1,26 @@
 package com.example.learnstate
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.learnstate.ui.theme.LearnStateTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +29,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LearnStateTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    LearnState()
                 }
             }
         }
@@ -31,17 +41,20 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun LearnState(){
+    //var age = 0
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LearnStateTheme {
-        Greeting("Android")
+    // desta forma, é possivel incrementar o valor mudar ao mesmo tempo na interface
+    var age by remember {
+        mutableStateOf(0)
+    }
+
+    Column {
+        Button(onClick = {age ++
+            //Log.v("TAG", "age "+age)
+        }, Modifier.padding(vertical = 40.dp)
+        ) {
+            Text(text = "Eu tenho $age anos de idade.")
+        }
     }
 }
