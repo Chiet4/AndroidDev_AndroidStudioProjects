b2434d9226c9f3f9a21efbf91e020af3cb6eb9b0
Anchieta Albano <111232477+Chiet4@users.noreply.github.com>
2024-10-28 15:59:49 -0300

    create LoginActivity com Jetpack compose

---
+++ JetpackComposeMiniProjeto/app/src/main/java/com/example/jetpackcomposeminiprojeto/LoginActivity.kt
@@ -0,0 +1,174 @@
+package com.example.jetpackcomposeminiprojeto
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
+
+@Composable
+@Preview
+fun LoginScreen(){
+    val scrillState = rememberScrollState()
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+            .verticalScroll(scrillState)
+    ) {
+        Image(
+           painter = painterResource(R.drawable.top_2),
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxSize()
+                .height(200.dp),
+            contentScale = ContentScale.Crop
+        )
+        Text(
+            text = "JCProject",
+            fontSize = 50.sp,
+            fontWeight = FontWeight.Bold,
+            color = colorResource(R.color.orange),
+            modifier = Modifier
+                .padding(top = 16.dp)
+                .fillMaxWidth(),
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = "Inicie suas atividades",
+            fontSize = 14.sp,
+            modifier = Modifier
+                .padding(top = 16.dp)
+                .fillMaxWidth(),
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = "Entre com seu login",
+            fontSize = 25.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(top = 16.dp)
+                .fillMaxWidth(),
+            textAlign = TextAlign.Center
+        )
+
+        // Captura de dados
+        OutlinedTextField(
+            value = "",
+            onValueChange = {},
+            label = {(Text(text = "Seu email"))},
+            modifier = Modifier
+                .padding(horizontal = 16.dp, vertical = 12.dp)
+                .fillMaxWidth(),
+            shape = RoundedCornerShape(10.dp),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email)
+        )
+
+        OutlinedTextField(
+            value = "",
+            onValueChange = {},
+            label = {(Text(text = "Sua senha"))},
+            modifier = Modifier
+                .padding(horizontal = 16.dp, vertical = 12.dp)
+                .fillMaxWidth(),
+            shape = RoundedCornerShape(10.dp),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+        )
+
+        Text(
+            text = "Esqueceu a senha? Recupere aqui",
+            fontSize = 14.sp,
+            modifier = Modifier
+                .padding(top = 32.dp)
+                .fillMaxWidth(),
+            textAlign = TextAlign.Center
+        )
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .padding(horizontal = 16.dp, vertical = 32.dp)
+                .fillMaxWidth()
+        ) {
+            Divider(modifier = Modifier.weight(1f), color = Color.Gray)
+                Text(
+                    text = "Ou faça login com",
+                    fontSize = 16.sp,
+                    modifier = Modifier
+                        .padding(top = 24.dp)
+                )
+            Divider(modifier = Modifier.weight(1f), color = Color.Gray)
+        }
+
+        Button(
+            onClick = {},
+            modifier = Modifier
+                .padding(horizontal = 16.dp, vertical = 16.dp)
+                .fillMaxWidth()
+                .height(50.dp),
+            shape = RoundedCornerShape(10.dp),
+            colors = ButtonDefaults.buttonColors(containerColor = colorResource(R.color.blue))
+        ) {
+            Text(
+                text = "Login",
+                fontSize = 24.sp,
+                color = Color.White,
+                fontWeight = FontWeight.Bold
+            )
+        }
+
+        Text(
+            text = "Não tem conta?",
+            fontSize = 14.sp,
+            modifier = Modifier
+                .padding(top = 16.dp)
+                .fillMaxWidth(),
+            textAlign = TextAlign.Center
+        )
+
+        Text(
+            text = "Criar uma",
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold,
+            color = colorResource(R.color.blue),
+            modifier = Modifier
+                .padding(top = 16.dp, bottom =16.dp)
+                .fillMaxWidth(),
+            textAlign = TextAlign.Center
+        )
+
+    }
+}
